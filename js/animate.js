7d9ce924fa51b038e69ce269de54140a8c1a599e
Ilya-Saldeckiy <melllllowtoy@yandex.ru>
2021-08-30 16:58:22 +0300

    fix pages

--- js/animate.js
+++ js/animate.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
             top = $(id).offset().top;
         $('body,html').animate(1500);
     });
+	$(".header__btn").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate(1500);
+	});
 });
--- js/mask.js
+++ js/mask.js
@@ -7,3 +7,8 @@ let selector2 = document.querySelectorAll('.email__input');
 let im2 = new Inputmask('*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}].*{1,20}');
 im2.mask(selector2);
 
+
+
+let selector3 = document.querySelectorAll('.first-test__input');
+let im3 = new Inputmask('999');
+im3.mask(selector3);
