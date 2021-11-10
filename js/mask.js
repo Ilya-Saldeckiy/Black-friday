8789d30bb5958214c6867830533e8a78c778fc7c
Ilya-Saldeckiy <melllllowtoy@yandex.ru>
2021-11-10 16:17:20 +0300

    add landing page

--- js/mask.js
+++ js/mask.js
@@ -1,23 +1,51 @@
-let selector = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(selector);
+$("#email-1").inputmask({
+	mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+	showMaskOnHover: false,
+	showMaskOnFocus: true,
+	greedy: false,
+	onBeforePaste: function (pastedValue, opts) {
+		pastedValue = pastedValue.toLowerCase();
+		return pastedValue.replace("mailto:", "");
+	},
+	definitions: {
+		'*': {
+			validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+			casing: "lower"
+		}
+	}
+});
+
+$("#email-2").inputmask({
+	mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+	showMaskOnHover: false,
+	showMaskOnFocus: true,
+	greedy: false,
+	onBeforePaste: function (pastedValue, opts) {
+		pastedValue = pastedValue.toLowerCase();
+		return pastedValue.replace("mailto:", "");
+	},
+	definitions: {
+		'*': {
+			validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+			casing: "lower"
+		}
+	}
+});
 
 
-let selector2 = document.querySelectorAll('.email__input');
-let im2 = new Inputmask('*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}].*{1,20}');
-im2.mask(selector2);
-
-
-
-let selector3 = document.querySelectorAll('.first-test__input');
-let im3 = new Inputmask('999');
-im3.mask(selector3);
-
-
-let selector4 = document.querySelectorAll('.test__input');
-let im4 = new Inputmask('999');
-im4.mask(selector4);
-
-let selector5 = document.querySelectorAll('.input__9');
-let im5 = new Inputmask('99');
-im5.mask(selector5);
+$("#email-10").inputmask({
+	mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+	showMaskOnHover: false,
+	showMaskOnFocus: true,
+	greedy: false,
+	onBeforePaste: function (pastedValue, opts) {
+		pastedValue = pastedValue.toLowerCase();
+		return pastedValue.replace("mailto:", "");
+	},
+	definitions: {
+		'*': {
+			validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+			casing: "lower"
+		}
+	}
+});
